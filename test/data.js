742be388efcc72e241bb7c65e5d2863500f0c029
Mohan Grewis <mohan.grewis@epitech.eu>
2020-01-14 20:07:38 -0500

    Test

--- test/data.js
+++ test/data.js
@@ -7,32 +7,27 @@ const config = require('../config/database');
 let server;
 let token;
 
-before(function (done) {
-    User.deleteOne({email: "testemaildonotuse@gmail.com"}, function(err) {
-
-        request.post("http://localhost:3000/auth/signup").form({
-            name: "testnamedonotuse",
-            password: "testpassworddonotuse123",
-            email: "testemaildonotuse@gmail.com",
-            birthdate: "1998-10-08"
-        }).on('response', function(response) {
-            token = jwt.sign(newUser.toJSON(), config.secret);
-            done();
-        })
-    })
-
-})
-
 describe("Data routes test", function() {
     before()
     describe("Get Vehicue Infos", function () {
         it("Success", function() {
-            request.post({url:'http://localhost:3000/auth/getuserinfos', form: {token: token}}, function(err,httpResponse,body){ 
-                let parsedbody = JSON.parse(body)
-                expect(parsebody.success).to.equal(true);
-                
-                done();
-             })
+            User.deleteOne({email: "testemaildonotuse@gmail.com"}, function(err) {
+
+                request.post("http://localhost:3000/auth/signup").form({
+                    name: "testnamedonotuse",
+                    password: "testpassworddonotuse123",
+                    email: "testemaildonotuse@gmail.com",
+                    birthdate: "1998-10-08"
+                }).on('response', function(response) {
+                    token = jwt.sign(newUser.toJSON(), config.secret);
+                    request.post({url:'http://localhost:3000/auth/getuserinfos', form: {token: token}}, function(err,httpResponse,body){ 
+                        let parsedbody = JSON.parse(body)
+                        expect(parsebody.success).to.equal(true);
+                        done();
+                    })
+                })
+            })
+            
         })
         it("Speed", function() {
             request.post({url:'http://localhost:3000/auth/getuserinfos', form: {token: token}}, function(err,httpResponse,body){ 
