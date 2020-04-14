5e9adc2c5f958863081ac0ae41dca4b61cc044e7
Mohan Grewis <mohan.grewis@epitech.eu>
2020-04-14 10:44:51 -0400

    Adding routes to express

--- routes/data.js
+++ routes/data.js
@@ -2,9 +2,12 @@ const express = require('express')
 const router = express.Router();
 const getvehiculeinfo = require('./data/getvehiculeinfo.js');
 const receiveshock = require('./data/receiveshock.js');
+const uploaddrivingdata = require('./data/uploaddrivingdata.js');
 
 router.route('/getvehiculeinfo').post(getvehiculeinfo);
 
 router.route('/receiveshock').post(receiveshock);
+
+router.route('/uploaddrivingdata').post(uploaddrivingdata);
 
 module.exports = router;
