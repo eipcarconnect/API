c7a1d31e47d6682d7f7dfd1a24adb29afdf36294
Mohan Grewis <mohan.grewis@epitech.eu>
2020-04-14 10:34:40 -0400

    Adding uploaddriving data route

--- routes/data/uploaddrivingdata.js
+++ routes/data/uploaddrivingdata.js
@@ -4,8 +4,8 @@ const log = require('../log');
 const User = require('../../models/user');
 
 /**
- * @api {post} /data/receiveshock Get Vehicule Info
- * @apiName Receive Shock
+ * @api {post} /data/uploaddrivingdata Upload vehicule infos
+ * @apiName Get Vehicule Info
  * @apiGroup Data
  *
  * @apiParam {String} token The user token
@@ -13,15 +13,15 @@ const User = require('../../models/user');
  * @apiSuccess {Boolean} success true
  * @apiSuccess {String} speed  The speed is sent in km/h
  * @apiSuccess {String} fuel  The fuel is sent in %
- * @apiSuccess {String} latitude  The latitude of the vehicule
- * @apiSuccess {String} longitude  The longitude of the vehicule
- * @apiSuccess {String} latitude  The latitude of the vehicule
+ * @apiSuccess {String} latitude  The latitude of the vehicle
+ * @apiSuccess {String} longitude  The longitude of the vehicle
+ * @apiSuccess {String} latitude  The latitude of the vehicle
  *
  * @apiSuccessExample Success-Response:
  *     HTTP/1.1 200 OK
  *     {
- *			success: true,
- *			msg : "Shock notification sent"
+ *			success: true, 
+ *			msg: "Driving data successfully uploaded"
  *     }
  *
  * @apiError MissingArgument An argument of the request is missing
@@ -55,18 +55,18 @@ const User = require('../../models/user');
 module.exports = 
 function (req, res) {
 	if (!req.body) {
-		log("Body is empty", "INFO", "receiveshock.js");
+		log("Body is empty", "INFO", "uploaddrivingdata.js");
 		res.status(400);
 		return res.json({ success: false, error: 'BodyEmpty' });
 	} else if (!req.body.token) {
-		log("Body is empty", "INFO", "receiveshock.js");
+		log("Body is empty", "INFO", "uploaddrivingdata.js");
 		console.log(req.body);
 		res.status(400);
 		return res.json({ success: false, error: 'MissingArgument' });
 	} else {
 		jwt.verify(req.body.token, config.secret, function(err, decoded){
 			if (err) {
-				log("Invalid Token", "INFO", "receiveshock.js");
+				log("Invalid Token", "INFO", "uploaddrivingdata.js");
 				res.status(400);
 				return res.json({ success: false, error: 'InvalidToken' });
 			}
@@ -75,14 +75,14 @@ function (req, res) {
                     email: decoded.email
                 }, function(err, user) {
                     if (!user) {
-                        log("Invalid Token", "ERROR", "receiveshock.js");
+                        log("Invalid Token", "ERROR", "uploaddrivingdata.js");
                         res.status(500);
                         return res.json({success: false, error: 'InvalidToken'});
 					}
 					else {
-						log("Shock notification sent", "INFO", "receiveshock.js");
+						log("Driving data successfully uploaded", "INFO", "uploaddrivingdata.js");
 						res.status(201);
-						return res.json({success: true, msg : "Shock notification sent"});
+						return res.json({success: true, msg: "Driving data successfully uploaded"});
 					}
 				});
 				
