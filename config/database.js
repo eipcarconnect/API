eaa76e75e31684a478720c3a24411e187d47b0f0
Mohan Grewis <mohan.grewis@epitech.eu>
2020-05-20 16:20:05 -0400

    Retrieve config

--- config/database.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	'secret':'nodeauthsecret12345',
-	'database':'mongodb://apiuser:5tuipAx4dv@database-carconnect:27017/api',
-	'firebase':'firebase-adminsdk-hh0f7@carconnect-e09c5.iam.gserviceaccount.com'
-    };
