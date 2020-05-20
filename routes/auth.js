20b375ed0923e5a9bd07316c70e1c10eb513cffa
Mohan Grewis <mohan.grewis@epitech.eu>
2020-05-20 11:06:37 -0400

    Adding route for registration token

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@ const signup = require('./auth/signup.js');
 const signin = require('./auth/signin.js');
 const getuserinfos = require('./auth/getuserinfos.js');
 const edit = require('./auth/edit.js');
+const addregistrationtoken = require('./auth/addRegistrationToken');
 
 router.route('/edit').post(edit);
 
@@ -12,5 +13,7 @@ router.route('/signup').post(signup);
 router.route('/signin').post(signin);
 
 router.route('/getuserinfos').post(getuserinfos);
+
+router.route('/addregistrationtoken').post(addregistrationtoken);
 
 module.exports = router;
