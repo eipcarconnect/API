d83b9a19d190a086f8c11250279abc1bbe81c21a
Mohan Grewis <mohan.grewis@gmail.com>
2020-12-01 17:56:11 +0100

    Add get user route and kilometer in DB doc

--- doc/api_project.js
+++ doc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-11-30T17:39:13.077Z",
+    "time": "2020-12-01T16:56:03.897Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
